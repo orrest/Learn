6a1e69d6c8cb4be677cf89a8f554d01bb3b0e27c
xufeng9 <xufeng9@xiaomi.com>
2021-08-06 18:21:51 +0800

    init

--- src/ds/graph/TwoColor.java
+++ src/ds/graph/TwoColor.java
@@ -1,8 +1,7 @@
 package ds.graph;
 
 public class TwoColor {
-    // Test if gitignore is in effected.
     public static void main(String[] args) {
-        System.out.println("Test if gitignore is in effected.");
+
     }
 }
