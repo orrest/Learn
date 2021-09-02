fd6f40fe7adf0d567e9a1bdb676a87fd480bd9dd
xufeng9 <xufeng9@xiaomi.com>
2021-09-02 10:41:28 +0800

    4.1.17

--- src/ds/graph/exercises/GraphProperties.java
+++ src/ds/graph/exercises/GraphProperties.java
@@ -27,13 +27,15 @@ public class GraphProperties {
 
     private void computeGirth(Graph G){
         for (int v = 0; v < G.V(); v++){
-            Arrays.fill(marked, false);
-            Arrays.fill(distTo, 0);
             girth = Math.min(girth, bfsForGirth(G, v));
         }
     }
 
     private int bfsForGirth(Graph G, int s){
+        Arrays.fill(marked, false);
+        Arrays.fill(distTo, 0);
+        Arrays.fill(edgeTo, 0);
+
         int shortestCycle = Integer.MAX_VALUE;
 
         Queue<Integer> queue = new LinkedList<>();
@@ -62,13 +64,16 @@ public class GraphProperties {
     }
 
     /**
+     * @return 图中最短环的长度
+     */
+    public int girth(){return girth;}
+
+    /**
      * 找到所有顶点的离心率
      * @param G 一副图
      */
     private void allEccentricity(Graph G){
         for (int v = 0; v < G.V(); v++){
-            Arrays.fill(distTo, 0);
-            Arrays.fill(marked, false); // 忘记把marked重置了!
             e[v] = bfsForEccentricity(G, v);
         }
     }
@@ -80,6 +85,9 @@ public class GraphProperties {
      * @return 一个顶点的离心率
      */
     private int bfsForEccentricity(Graph G, int v){
+        Arrays.fill(distTo, 0);
+        Arrays.fill(marked, false); // 忘记把marked重置了!
+
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(v);
         marked[v] = true;
@@ -142,6 +150,7 @@ public class GraphProperties {
     }
 
     public static void main(String[] args) {
+        // 0-1-2-3-4-5-6-7-8-9-10
         Graph graph = new Graph(11);
         graph.addEdge(0 ,1);
         graph.addEdge(1 ,2);
@@ -153,15 +162,65 @@ public class GraphProperties {
         graph.addEdge(7 ,8);
         graph.addEdge(8 ,9);
         graph.addEdge(9 ,10);
+        System.out.println(graph);
 
         GraphProperties graphProperties = new GraphProperties(graph);
         StdOut.println("Diameter: " + graphProperties.diameter() + " Expected: 10");
         StdOut.println("Radius: " + graphProperties.radius() + " Expected: 5");
         StdOut.println("Center: " + graphProperties.center() + " Expected: 5");
 
-        for (int i = 0; i < graphProperties.e.length; i++){
-            System.out.println(i+" "+graphProperties.e[i]);
-        }
+
+        // ---------girth-----------
+//        graph.addEdge(10, 0);
+//        graph.addEdge(0,3);
+//        System.out.println(graph);
+//        GraphProperties在构造的时候就计算了，所以尽管加了边，
+//        但并没有重新计算，输出的仍然是最开始的图（0-...-10）的girth
+//        System.out.println(graphProperties.girth());
+
+        // Graph with girth = 3
+        Graph graph1 = new Graph(6);
+        graph1.addEdge(2 ,3);
+        graph1.addEdge(0 ,1);
+        graph1.addEdge(3 ,1);
+        graph1.addEdge(5 ,3);
+        graph1.addEdge(2 ,0);
+        graph1.addEdge(1 ,2);
+        graph1.addEdge(4 ,2);
+        graph1.addEdge(4 ,5);
+        graph1.addEdge(4 ,0);
+
+        // Graph with girth = 2
+        Graph graph2 = new Graph(4);
+        graph2.addEdge(0, 1);
+        graph2.addEdge(1, 0); //Parallel edge
+        graph2.addEdge(1, 2);
+        graph2.addEdge(2, 3);
+
+        // Graph with girth = 4
+        Graph graph3 = new Graph(4);
+        graph3.addEdge(0, 1);
+        graph3.addEdge(1, 2);
+        graph3.addEdge(2, 3);
+        graph3.addEdge(3, 0);
+
+        // Graph with girth = Integer.MAX_VALUE
+        Graph graph4 = new Graph(4);
+        graph4.addEdge(0, 1);
+        graph4.addEdge(1, 3);
+        graph4.addEdge(2, 3);
+
+        GraphProperties graphProperties1 = new GraphProperties(graph1);
+        StdOut.println("Girth 1: " + graphProperties1.girth() + " Expected: 3");
+
+        GraphProperties graphProperties2 = new GraphProperties(graph2);
+        StdOut.println("Girth 2: " + graphProperties2.girth() + " Expected: 2");
+
+        GraphProperties graphProperties3 = new GraphProperties(graph3);
+        StdOut.println("Girth 3: " + graphProperties3.girth() + " Expected: 4");
+
+        GraphProperties graphProperties4 = new GraphProperties(graph4);
+        StdOut.println("Girth 4: " + graphProperties4.girth() + " Expected: 2147483647");
     }
 }
 
