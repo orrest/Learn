a24eaf7eda907481b99a4b1b495e47b3d0e54db6
xufeng9 <xufeng9@xiaomi.com>
2021-11-12 20:44:47 +0800

    proxy pattern 1/n

---
+++ src/pers/xf/learn/designpattern/proxy/Test.java
@@ -0,0 +1,9 @@
+package pers.xf.learn.designpattern.proxy;
+
+import java.lang.reflect.InvocationHandler;
+
+public class Test {
+    public static void main(String[] args) {
+//        InvocationHandler
+    }
+}
